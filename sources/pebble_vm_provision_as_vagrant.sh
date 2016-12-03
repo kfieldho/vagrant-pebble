34dfdf596c16aa096fa19c02b980662314df31f5
Keith Fieldhouse <keith.fieldhouse@kitware.com>
2016-12-03 14:10:59 -0500

    Update Pebble-Tools to 4.5

--- sources/pebble_vm_provision_as_vagrant.sh
+++ sources/pebble_vm_provision_as_vagrant.sh
@@ -15,16 +15,16 @@ echo "---------------------------------------------------"
 echo "              Getting the pebble sdk               "
 echo "---------------------------------------------------"
 #wget -q -a /vagrant/pebble.log http://assets.getpebble.com.s3-website-us-east-1.amazonaws.com/sdk2/$PEBBLE_SDK.tar.gz
-wget -q -a /vagrant/peeble.log https://s3.amazonaws.com/assets.getpebble.com/pebble-tool/pebble-sdk-4.0-linux64.tar.bz2
+wget -q -a /vagrant/peeble.log https://s3.amazonaws.com/assets.getpebble.com/pebble-tool/pebble-sdk-4.5-linux64.tar.bz2
 
-tar -jxf pebble-sdk-4.0-linux64.tar.bz2
-rm pebble-sdk-4.0-linux64.tar.bz2
+tar -jxf pebble-sdk-4.5-linux64.tar.bz2
+rm pebble-sdk-4.5-linux64.tar.bz2
 chown -R vagrant:vagrant /home/vagrant/pebble-dev
 echo "---------------------------------------------------"
 echo "                Setting up profile                 "
 echo "---------------------------------------------------"
-echo "export PATH=/home/vagrant/pebble-dev/pebble-sdk-4.0-linux64/bin:\$PATH" >> /home/vagrant/.profile
-cd /home/vagrant/pebble-dev/pebble-sdk-4.0-linux64
+echo "export PATH=/home/vagrant/pebble-dev/pebble-sdk-4.5-linux64/bin:\$PATH" >> /home/vagrant/.profile
+cd /home/vagrant/pebble-dev/pebble-sdk-4.5-linux64
 echo "---------------------------------------------------"
 echo "               Getting ubuntu tools                "
 echo "---------------------------------------------------"
